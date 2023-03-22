4cf7e0dc2f413a43339a40c7ef83e56a3f852ae8
moyel <1010404561@qq.com>
2023-03-22 11:20:05 +0800

    导出 server

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const Rtsp2WsServer = require("./src/utils/Rtsp2WsServer");
-
-const rtsp2ws = new Rtsp2WsServer({ffmpeg: "D:\\pgs\\ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe"});
-
-rtsp2ws.listen();
+module.exports = require("./src/utils/Rtsp2WsServer");
